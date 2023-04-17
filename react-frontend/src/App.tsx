afeebc9aa3e5451982593ce9997e94560943ee97
andrelivramento <andregoncalves97@gmail.com>
2023-04-17 16:22:07 +0200

    init admin page and enable cors on lobbyController

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import {AdminPage} from "./AdminPage";
 
 function App() {
 
   return (
     <div className="App">
         <h1>Hello World</h1>
+        <AdminPage></AdminPage>
     </div>
   )
 }
