b94bb2537abb45bae721be52412afda807ee783e
TechnoTurtle777 <techno.turtle777@gmail.com>
2023-04-19 09:52:44 +0200

    [View game board UC] Correctly space the column coordinate index of the game board

--- react-frontend/src/game/board/TablutBoard.tsx
+++ react-frontend/src/game/board/TablutBoard.tsx
@@ -7,40 +7,46 @@ type TablutBoardProps = {
 
 export default function TablutBoard({board}: TablutBoardProps) {
     const Row = styled.div`
-    display: flex;
+      display: flex;
     `;
     const TablutRow = styled.div`
-    display: flex;
-    margin: 0 auto;
+      display: flex;
+      margin: 0 auto;
     `;
     const RowIndex = styled.span`
-    margin: 0.5em;
-    align-self: center;
+      margin: 0.5em;
+      align-self: center;
     `;
     const ColIndex = styled.span`
-    margin: 0.5em;
-    align-self: center;
+      margin: 0.5em;
+      align-self: center;
     `;
     const ColumnIndexContainer = styled.div`
-    display: flex;
-    margin: 0 auto;
-    flex-direction: row;
-    justify-content: evenly;
-    width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      width: 100%;
+      margin-left: 0.5em;
     `;
     const TablutBoardContainer = styled.div`
-    flex-wrap: wrap;
-    margin: 0 auto;
-    padding: 10px;
-    width: auto;
+      flex-wrap: wrap;
+      padding: 10px;
+      width: fit-content;
     `;
+
+    function columnCoordinateOf(colIndex: number) {
+
+        return (
+            String.fromCharCode(65 + colIndex)
+        );
+    }
 
     return (
         <TablutBoardContainer>
             <ColumnIndexContainer>
                 {board[0].map((_, colIndex) => (
                     <ColIndex>
-                        {String.fromCharCode(65 + colIndex)}
+                        {columnCoordinateOf(colIndex)}
                     </ColIndex>
                 ))}
             </ColumnIndexContainer>
