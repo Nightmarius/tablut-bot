08e29778913479a382431808753c74f1d2ecc293
grosp0010 <grosp0010@gmail.com>
2023-04-19 08:34:43 +0200

    made small improvements and added styling

--- react-frontend/src/rules/Example.tsx
+++ react-frontend/src/rules/Example.tsx
@@ -1,12 +1,8 @@
-function Example({exampleText}: Props) {
+export default function Example({exampleText}: Props) {
     return (
         <div>
-            <h4>Examples</h4>
             <p>{exampleText}</p>
-            <h5>Before</h5>
-            <p>INSERT BOARD HERE</p>
-            <h5>After</h5>
-            <p>INSERT BOARD HERE</p>
+            <p>INSERT BOARD HERE WITH AMAZING ANIMATION</p>
         </div>
     );
 
@@ -15,5 +11,3 @@ function Example({exampleText}: Props) {
 interface Props {
     exampleText: string;
 }
-
-export default Example;
