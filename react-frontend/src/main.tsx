79162ddbdd14e838e7f13af4a16c3924e4f74007
Konrad, Andres <andres.konrad@zuehlke.com>
2023-04-20 11:48:15 +0000

    Applied suggestion

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <GamePage/>,
     },
     {
-        path: "/tournament/:leaderboard",
+        path: "/leaderboard",
         element: <LeaderboardPage/>,
     },
     {
