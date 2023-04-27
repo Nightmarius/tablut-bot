a47dc59cbabe556cfbb5f2bde1d4026cc47d5e86
anko <andres.konrad@hotmail.com>
2023-04-27 22:33:54 +0200

    [ZTOPCHA-1] use env variable for backend url in RemoteService.tsx

--- react-frontend/src/services/RemoteService.tsx
+++ react-frontend/src/services/RemoteService.tsx
@@ -7,7 +7,7 @@ class RemoteService {
 
     constructor() {
         this.instance = axios.create({
-            baseURL: 'http://localhost:8080',
+            baseURL: import.meta.env.VITE_BACKEND_URL,
             timeout: 5000,
             headers: {
                 'Content-Type': 'application/json',
