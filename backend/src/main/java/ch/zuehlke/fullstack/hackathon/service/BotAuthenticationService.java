b754e97231b85be408313792ac9e098e76629497
Marius Niklaus <marius.niklaus@zuehlke.com>
2023-04-28 16:17:29 +0200

    add service annotation

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/BotAuthenticationService.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/BotAuthenticationService.java
@@ -3,8 +3,10 @@ package ch.zuehlke.fullstack.hackathon.service;
 import ch.zuehlke.common.BotDto;
 import ch.zuehlke.fullstack.hackathon.controller.AuthenticationResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Service
 public class BotAuthenticationService {
     private final BotService botService;
 
