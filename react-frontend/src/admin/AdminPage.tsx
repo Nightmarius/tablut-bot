8c95db81a1f2f7844feb39f9e345116d5ce5a741
Marius Niklaus <marius.niklaus@zuehlke.com>
2023-05-04 11:19:25 +0200

    style admin page

--- react-frontend/src/admin/AdminPage.tsx
+++ react-frontend/src/admin/AdminPage.tsx
@@ -2,6 +2,32 @@ import { useEffect, useState } from "react";
 import remoteService from "../services/RemoteService";
 import { useNavigate } from "react-router";
 import { BotDto } from "../shared/domain/model";
+import Button from "../shared/ui/button/Button";
+import styled from "styled-components";
+import Input from "../shared/ui/input/input";
+
+const StyledRow = styled.div`
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: 1fr auto;
+    height: 4%;
+    width: 100%;
+    padding-bottom: 4px;
+    padding-top: 4px;
+    border-bottom: 1px solid var(--quinary);
+`;
+
+const Title = styled.h2`
+    font-size: 2rem;
+    background: linear-gradient(90deg, #985b9c, #a6e0fe);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`;
+
+const StyledButton = styled.button`
+    width: 50px;
+    margin-left: 10px;
+`;
 
 export function AdminPage() {
     interface GameId {
@@ -52,16 +78,17 @@ export function AdminPage() {
         <>
             <button onClick={handleCreateGame}>New Game</button>
             <GameList gameIds={gameIds} />
-            {warning != null && <p>{warning}</p>}
-            <input type="text" placeholder="Name" value={name} onChange={(e) => handleNameChange(e.target.value)} />
-            <button onClick={generateToken}>Generate Access token</button>
+            <Title>Access Tokens</Title>
             {bots.map((bot) => (
-                <button onClick={() => navigator.clipboard.writeText(bot.token.value)}>
-                    <li>
-                        {bot.name.value}: {bot.token.value}
-                    </li>
-                </button>
+                <StyledRow>
+                    <div>{bot.name.value}</div>
+                    <div>{bot.token.value}</div>
+                    <StyledButton onClick={() => navigator.clipboard.writeText(bot.token.value)}>Copy</StyledButton>
+                </StyledRow>
             ))}
+            {warning != null && <p>{warning}</p>}
+            <Input type="text" placeholder="Name" value={name} onChange={(e) => handleNameChange(e.target.value)} />
+            <Button onClick={generateToken}>Generate Access token</Button>
         </>
     );
 }
