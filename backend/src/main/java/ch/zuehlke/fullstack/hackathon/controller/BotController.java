d7b2f66a3bc8c77204bc6cdf013d1f843d876c4e
Marius Niklaus <marius.niklaus@zuehlke.com>
2023-05-04 17:54:56 +0200

    add lobby service

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/controller/BotController.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/controller/BotController.java
@@ -1,18 +1,13 @@
 package ch.zuehlke.fullstack.hackathon.controller;
 
 import ch.zuehlke.common.*;
-import ch.zuehlke.fullstack.hackathon.service.BotAuthenticationService;
-import ch.zuehlke.fullstack.hackathon.service.GameService;
-import ch.zuehlke.fullstack.hackathon.service.NotificationService;
-import ch.zuehlke.fullstack.hackathon.service.TournamentService;
+import ch.zuehlke.fullstack.hackathon.service.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -21,60 +16,29 @@ import java.util.Optional;
 public class BotController {
 
     private final BotAuthenticationService botAuthService;
+    private final LobbyService lobbyService;
     private final GameService gameService;
     private final TournamentService tournamentService;
     private final NotificationService notificationService;
 
-    @Operation(summary = "Joins a game",
-            description = "Joins a game and returns the socket url")
-    @ApiResponse(responseCode = "200", description = "Successfully joined the game")
-    @ApiResponse(responseCode = "400", description = "Game is already full")
+    @Operation(summary = "Bot joins",
+            description = "Bot joins the lobby")
+    @ApiResponse(responseCode = "200", description = "Successfully joined the lobby")
     @ApiResponse(responseCode = "403", description = "Bot credentials invalid")
-    @ApiResponse(responseCode = "404", description = "The game does not exist")
-    @PostMapping("/game/{gameId}/join")
-    public ResponseEntity<JoinResponse> joinGame(@PathVariable int gameId, @RequestHeader("token") String token, @RequestBody JoinRequest joinRequest) {
+    @PostMapping("/lobby/join")
+    public ResponseEntity<JoinResponse> join(@RequestHeader("token") String token, @RequestBody JoinRequest joinRequest) {
         BotDto bot = new BotDto(joinRequest.name(), new Token(token));
         AuthenticationResult result = botAuthService.authenticate(bot);
         if (result == AuthenticationResult.DENIED) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
-        JoinResult joinResult = gameService.join(gameId, joinRequest.name());
-        if (joinResult.resultType() == JoinResult.JoinResultType.GAME_NOT_FOUND) {
-            return ResponseEntity.notFound().build();
-        }
-        if (joinResult.resultType() == JoinResult.JoinResultType.GAME_FULL) {
-            return ResponseEntity.badRequest().build();
-        }
+        JoinResult joinResult = lobbyService.join(joinRequest.name());
 
-        notificationService.notifyGameUpdate(new GameId(gameId));
-        return ResponseEntity.ok(new JoinResponse(joinResult.playerId()));
+        notificationService.notifyLobbyUpdate();
+        return ResponseEntity.ok(new JoinResponse(joinResult.name()));
     }
 
-    @Operation(summary = "Joins a tournament",
-            description = "Joins a tournament and returns the socket url")
-    @ApiResponse(responseCode = "200", description = "Successfully joined the tournament")
-    @ApiResponse(responseCode = "400", description = "Tournament is already full")
-    @ApiResponse(responseCode = "404", description = "The tournament does not exist")
-    @PostMapping("/tournament/{tournamentId}/join")
-    public ResponseEntity<JoinResponse> joinTournament(@PathVariable int tournamentId, @RequestHeader String token, @RequestBody JoinRequest joinRequest) {
-        BotDto bot = new BotDto(joinRequest.name(), new Token(token));
-        AuthenticationResult result = botAuthService.authenticate(bot);
-        if (result == AuthenticationResult.DENIED) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-
-        var joinResult = tournamentService.join(tournamentId, joinRequest.name());
-
-        if (joinResult.resultType() == TournamentJoinResult.TournamentJoinResultType.TOURNAMENT_NOT_FOUND) {
-            return ResponseEntity.notFound().build();
-        }
-        if (joinResult.resultType() == TournamentJoinResult.TournamentJoinResultType.TOURNAMENT_FULL) {
-            return ResponseEntity.badRequest().build();
-        }
-        notificationService.notifyTournamentUpdate(new TournamentId(tournamentId));
-        return ResponseEntity.ok(new JoinResponse(joinResult.playerId()));
-    }
 
     @Operation(summary = "Plays a move",
             description = "Plays a move")
@@ -84,11 +48,7 @@ public class BotController {
     @PostMapping("/game/{gameIdInt}/play")
     public ResponseEntity<Void> play(@PathVariable int gameIdInt, @RequestHeader String token, @RequestBody Move move) {
         GameId gameId = new GameId(gameIdInt);
-        Optional<PlayerName> name = gameService.getPlayerName(gameIdInt, move.playerId());
-        if (name.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-        BotDto bot = new BotDto(name.get(), new Token(token));
+        BotDto bot = new BotDto(move.name(), new Token(token));
         AuthenticationResult result = botAuthService.authenticate(bot);
         if (result == AuthenticationResult.DENIED) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
--- react-frontend/src/shared/domain/helper.tsx
+++ react-frontend/src/shared/domain/helper.tsx
@@ -2,7 +2,7 @@ import { GameAction, GameDto, GameStatus, Player } from "./model";
 
 export const getPlayerForNextTurn = (game: GameDto | undefined): Player | undefined => {
     if (game?.status === GameStatus.IN_PROGRESS && game.state.currentRequests.length > 0) {
-        return game.players.find((player) => player.id?.value === game.state.currentRequests[0].playerId?.value);
+        return game.players.find((player) => player.id?.value === game.state.currentRequests[0].playerName?.value);
     }
     return undefined;
 };
