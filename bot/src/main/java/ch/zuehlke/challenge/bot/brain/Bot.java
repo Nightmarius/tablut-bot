741301aa0ee80f750b916adf9d7d6a4d01057155
anko <andres.konrad@hotmail.com>
2023-06-01 10:54:36 +0200

    [internal] move InternalGameState to common so that bots brains can be locally tested

--- bot/src/main/java/ch/zuehlke/challenge/bot/brain/Bot.java
+++ bot/src/main/java/ch/zuehlke/challenge/bot/brain/Bot.java
@@ -1,11 +1,11 @@
 package ch.zuehlke.challenge.bot.brain;
 
-import ch.zuehlke.common.GameAction;
 import ch.zuehlke.common.Board;
+import ch.zuehlke.common.GameAction;
 
 import java.util.Set;
 
 public interface Bot {
 
-    GameAction decide(boolean attacker, Board board, Set<GameAction> possibleActions);
+    GameAction decide(boolean isAttackersTurn, Board board, Set<GameAction> possibleActions);
 }
